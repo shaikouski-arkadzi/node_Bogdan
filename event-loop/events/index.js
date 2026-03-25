ffb2030d146142eb4166087a4ecd2f1c30631cc8
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-25 22:35:05 +0300

    Add fs to event loop

--- event-loop/events/index.js
+++ event-loop/events/index.js
@@ -1,9 +1,30 @@
+const fs = require("fs");
+
 console.log("Program start");
 
 // Timeouts
 setTimeout(() => console.log("Timeout 1"), 0);
 setTimeout(() => console.log("Timeout 2"), 10);
 
+// Poll Phase
+fs.writeFile("event-loop/events/test.txt", "Hello Node.js", () =>
+  console.log("File wrutten"),
+);
+
 Promise.resolve().then(() => console.log("Promise 1"));
 
 console.log("Program end");
+
+// Sync
+//  ├── Program start
+//  └── Program end
+
+// Event Loop
+
+// Iteration 1
+//  ├── Microtasks → Promise 1
+//  ├── Timers → Timeout 1
+//  ├── Poll → File wrutten
+
+// Iteration 2
+//  └── Timers → Timeout 2
