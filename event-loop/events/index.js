6d46cbda11739e23ee589e1d3b7cf11508e3f34e
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-26 08:57:47 +0300

    Add DNS. More info about poll phase

--- event-loop/events/index.js
+++ event-loop/events/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const dns = require("dns");
 
 console.log("Program start");
 
@@ -11,7 +12,7 @@ setTimeout(() => {
 
 // Poll Phase
 fs.writeFile("event-loop/events/test.txt", "Hello Node.js", () =>
-  console.log("File wrutten"),
+  console.log("File written"),
 );
 
 Promise.resolve().then(() => console.log("Promise 1"));
@@ -20,10 +21,24 @@ process.nextTick(() => console.log("Next tick 1"));
 
 setImmediate(() => console.log("Immediate 1"));
 
+dns.lookup("localhost", (error, address, family) => {
+  console.log("DNS 1 localhost ", address);
+});
+
 console.log("Program end");
 
 // Sync
 //  ├── Program start
+//  ├── fs.writeFile()
+//  │      ├── создаётся FS request
+//  │      ├── регистрируется callback
+//  │      ├── отправляется в libuv
+//  │      └── Thread pool → начинает писать файл
+//  ├── dns.lookup()
+//  │      ├── создаётся DNS request
+//  │      ├── регистрируется callback
+//  │      ├── отправляется в libuv
+//  │      └── Thread pool → поиск localhost
 //  └── Program end
 
 // Event Loop
@@ -32,10 +47,15 @@ console.log("Program end");
 //  ├── Next tick → Next tick 1
 //  ├── Microtasks → Promise 1
 //  ├── Timers → Timeout 1
-//  ├── Poll → (fs ещё пишет файл)
+//  ├── Poll
+//  │     ├── проверка Thread pool
+//  │     ├── fs.writeFile
+//  │     │     └── File written
+//  │     └── dns.lookup
+//  │          └── ещё выполняется
 //  ├── Check → Immediate 1
-//  ├── Poll → File wrutten
 
 // Iteration 2
 //  ├── Timers → Timeout 2
-//  └── Next tick → Next tick 2
+//  ├── Next tick → Next tick 2
+//  └── Poll → DNS 1
