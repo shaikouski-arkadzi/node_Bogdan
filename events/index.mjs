2eeb93f847a2c972189a90883eb8bf58d5dae61f
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-30 21:34:58 +0300

    Add EventEmitter example with single event

--- events/index.mjs
+++ events/index.mjs
@@ -7,3 +7,8 @@ myEmitter.on("timeout", (seconds) =>
 );
 
 setTimeout(() => myEmitter.emit("timeout", 1), 1000);
+
+myEmitter.once("singleEvent", () => console.log("Single event"));
+
+setTimeout(() => myEmitter.emit("singleEvent"), 1500);
+setTimeout(() => myEmitter.emit("singleEvent"), 2000);
