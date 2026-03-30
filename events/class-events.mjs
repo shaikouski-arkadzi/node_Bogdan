4c89615abc7738d98d418045f603f95f0b539ed3
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-30 22:32:01 +0300

    Add eventEmitter example in class

---
+++ events/class-events.mjs
@@ -0,0 +1,23 @@
+import { EventEmitter } from "events";
+
+class Post extends EventEmitter {
+  constructor(name, text) {
+    super();
+    this.name = name;
+    this.text = text;
+    this.likes = 0;
+  }
+
+  like() {
+    this.likes += 1;
+    this.emit("likePost");
+  }
+}
+
+const myPost = new Post("title 1", "text 1");
+
+myPost.on("likePost", () => console.log("Like!"));
+
+console.log(myPost.likes);
+myPost.like();
+console.log(myPost.likes);
