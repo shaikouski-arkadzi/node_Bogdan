84b16519b102a1782146a1f3028671bf89cf204c
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-30 21:38:06 +0300

    Remove listener from event EventEmitter

--- events/index.mjs
+++ events/index.mjs
@@ -2,9 +2,10 @@ import { EventEmitter } from "events";
 
 const myEmitter = new EventEmitter();
 
-myEmitter.on("timeout", (seconds) =>
-  console.log(`Timeout event ${seconds} seconds!`),
-);
+const timeoutListener = (seconds) =>
+  console.log(`Timeout event ${seconds} seconds!`);
+
+myEmitter.on("timeout", timeoutListener);
 
 setTimeout(() => myEmitter.emit("timeout", 1), 1000);
 
@@ -12,3 +13,7 @@ myEmitter.once("singleEvent", () => console.log("Single event"));
 
 setTimeout(() => myEmitter.emit("singleEvent"), 1500);
 setTimeout(() => myEmitter.emit("singleEvent"), 2000);
+
+setTimeout(() => myEmitter.off("timeout", timeoutListener), 3000);
+
+setTimeout(() => myEmitter.emit("timeout", 1), 4000);
