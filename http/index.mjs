91b5d0ab89556b74334430890a6d7f6bc6eea8b2
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-03-31 21:36:42 +0300

    Add different paths

--- http/index.mjs
+++ http/index.mjs
@@ -1,10 +1,25 @@
 import http from "http";
 
+const obj = [
+  { id: 1, text: "Test 1" },
+  { id: 2, text: "Test 2" },
+  { id: 3, text: "Test 3" },
+];
+
 const server = http.createServer((req, res) => {
   console.log(req);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello");
+  if (req.url === "/text") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello");
+    return;
+  }
+  if (req.url === "/json") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(obj));
+    return;
+  }
 });
 
 server.listen(3002, () => console.log("Server was launced"));
