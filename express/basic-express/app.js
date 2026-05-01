902448a90967a82c3e9450f9e336330a7ee01bfb
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-05-01 14:58:03 +0300

    Add two handlers in one route

--- express/basic-express/app.js
+++ express/basic-express/app.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Response from Express"));
+const firstHandler = (req, res, next) => {
+  console.log("Response from Express");
+  next();
+};
+const secondHandler = (req, res) => res.send("Second handler");
+
+app.get("/", firstHandler, secondHandler);
 
 app.listen(3005, () => console.log("Server was started on port 3005"));
