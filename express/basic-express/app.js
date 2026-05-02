f657b0819e178c405672a72d3a98c427449a169b
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-05-02 07:30:51 +0300

    Add several methods on one route

--- express/basic-express/app.js
+++ express/basic-express/app.js
@@ -8,6 +8,12 @@ const firstHandler = (req, res, next) => {
 };
 const secondHandler = (req, res) => res.send("Second handler");
 
+const getCommentsHandler = (req, res) => res.send("Get comments");
+const postCommentsHandler = (req, res) => res.send("Post comments");
+
 app.get("/", firstHandler, secondHandler);
+
+app.get("/comments", getCommentsHandler);
+app.post("/comments", postCommentsHandler);
 
 app.listen(3005, () => console.log("Server was started on port 3005"));
