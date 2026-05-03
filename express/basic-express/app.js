b2bda403ec29e23e42789a2f7f358b0470308c4c
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-05-03 22:51:35 +0300

    Add getting request params

--- express/basic-express/app.js
+++ express/basic-express/app.js
@@ -11,9 +11,14 @@ const secondHandler = (req, res) => res.send("Second handler");
 const getCommentsHandler = (req, res) => res.send("Get comments");
 const postCommentsHandler = (req, res) => res.send("Post comments");
 
+const getCommentHandler = (req, res) => {
+  res.send(`Get comment. CommentId ${req.params.commentId}`);
+};
+
 app.get("/", firstHandler, secondHandler);
 
 app.get("/comments", getCommentsHandler);
 app.post("/comments", postCommentsHandler);
+app.get("/comments/:commentId", getCommentHandler);
 
 app.listen(3005, () => console.log("Server was started on port 3005"));
